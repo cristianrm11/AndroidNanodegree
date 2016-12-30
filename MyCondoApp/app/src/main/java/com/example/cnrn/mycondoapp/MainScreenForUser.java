a557236fe13cb1a10b672ad583752fe4c8f9b341
Cristian Retana <cnrn@gft.com>
2016-12-30 06:28:19 -0600

    Single Screen App

---
+++ MyCondoApp/app/src/main/java/com/example/cnrn/mycondoapp/MainScreenForUser.java
@@ -0,0 +1,19 @@
+package com.example.cnrn.mycondoapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainScreenForUser extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen_for_user);
+    }
+}
